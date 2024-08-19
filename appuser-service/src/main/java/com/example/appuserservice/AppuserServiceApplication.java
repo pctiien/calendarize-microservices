8a62506e4368ba424bff29f1d433eaf6274a9be6
pctien <pctien1401@gmail.com>
2024-08-19 16:01:15 +0700

    init project

---
+++ appuser-service/src/main/java/com/example/appuserservice/AppuserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppuserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppuserServiceApplication.class, args);
+    }
+
+}
